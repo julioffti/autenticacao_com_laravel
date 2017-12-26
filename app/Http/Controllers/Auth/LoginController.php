8d4d7810fd20534bc644f7a3a0eef0c6e200ab18
julio <julio_filgueiras@live.com>
2017-12-26 01:02:50 -0200

    poder logar com email, cpf ou telefone

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,26 +9,13 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
-
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
     protected $redirectTo = '/home';
-
     /**
      * Create a new controller instance.
      *
@@ -39,18 +26,40 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function redirectTo(){
+    public function redirectTo()
+    {
         return \Auth::user()->role == User::ROLE_ADMIN ? '/admin/home' : '/home';
     }
-
     public function logout(Request $request)
     {
         $this->guard()->logout();
-
         $request->session()->flush();
-
-        $request->session()->invalidate();
-
+        $request->session()->regenerate();
         return redirect($request->is('admin/*') ? '/admin/login' : '/login');
+    }
+    protected function credentials(Request $request)
+    {
+        $data = $request->only($this->username(),'password');
+        //$data['phone'] = $data['email'];
+        $usernameKey = $this->usernameKey();
+        if($usernameKey != $this->username()) {
+            $data[$this->usernameKey()] = $data[$this->username()];
+            unset($data[$this->username()]);
+        }
+        return $data;
+    }
+    protected function usernameKey()
+    {
+        $email = \Request::get('email'); //email,phone,cpf
+        $validator = \Validator::make([
+            'email' => $email
+        ], ['email' => 'cpf']);
+        if (!$validator->fails()) {
+            return 'cpf';
+        }
+        if (is_numeric($email)) {
+            return 'phone';
+        }
+        return 'email';
     }
 }
